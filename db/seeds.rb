0ad8335d4beccdc40e8c9f9f849726e95e434876
reireynoso <reireynoso@gmail.com>
2020-01-14 09:47:25 -0500

    added seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+announcement = Announcement.create(content: "First Announcement")
+
+teacher = Teacher.create(username: "Teacher")
+
+student = Student.create(username: "Student")
+
+byebug
+
+puts 'seeded'
