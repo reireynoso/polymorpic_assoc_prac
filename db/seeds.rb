9c2388bb88f426f4e4fde4bd05238182e4fafd7a
reireynoso <reireynoso@gmail.com>
2020-01-14 13:47:15 -0500

    added destroy_all

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Comment.destroy_all
+Announcement.destroy_all
+Student.destroy_all 
+Teacher.destroy_all
 
 announcement = Announcement.create(content: "First Announcement")
 
